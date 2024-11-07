9710bfa8ceff22606a3b2de4cda25560d90b7926
gautam dheer <gautam.litesh@gmail.com>
2024-11-07 14:49:45 +0530

    navbar done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,26 +7,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between item-center py-4 border-b border-white/10">
             <div>
-                <a href=""><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Pixel Positions"></a>
+                <a href="/"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Pixel Positions"></a>
              </div>
-            <div>
-                <a href="/">Home</a>
-                <a href="/jobs">Jobs</a>
+            <div class="space-x-6 font-bold">
+                <a href="/">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salary</a>
+                <a href="#">Companies</a>
             </div>
             <div>
-                Post a Job
+                <a href="">Post a Job</a>
             </div>
 
         </nav>
-        <main>
-            @yield('slot')
+        <main class="mt-10 max-w-[968px]">
+            {{ $slot }}        
         </main>
     </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,22 @@
 <x-layout>
-    <h1>Welcome to Pixel Positions</h1>
+  <section>
+    <h3>Top Jobs</h3>
+    <div>
+            <div>Laracast </div>
+            <div> 
+                <h3>View Producer</h3>
+                <p>Full Time - From $60,000</p>
+            </div>
+            <div> 
+              <div>
+                <a href="#">Tag</a>
+                <a href="#">Tag</a>
+                <a href="#">Tag</a>
+                <a href="#">Tag</a>
+                <a href="#">Tag</a>
+              </div>
+              <img src="http://placehold.it/42/42" alt="">
+            </div>
+    </div>
+  </section>
 </x-layout>
