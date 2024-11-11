a8671d4c1d5db16f5bd634f6d5e8dc0757c970dc
gautam dheer <gautam.litesh@gmail.com>
2024-11-12 00:42:17 +0530

    changes to web and welcome file

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/jobs', function () {
     return view('jobs.index');
 });
+
+ 
